656ce6081423043ba37ee42a42f90ab79e1f6529
aqsa <abid.aqsa26@gmail.com>
2019-04-29 22:05:49 +0500

    crud3

---
+++ ELMS/ELMS/Controllers/EmployeeController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanManagementSystem.Models;
+namespace LoanManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+
+        {
+            DB56Entities db = new DB56Entities();
+            var laa = db.Employees.ToList();
+            List<Employeemodel> l = new List<Employeemodel>();
+            foreach (var a in laa)
+            {
+                Employeemodel la = new Employeemodel();
+                la.CNIC = a.CNIC;
+                la.Name = a.Name;
+                la.Password = a.Password;
+                la.Email = a.Email;
+                la.Salary = a.Salary;
+                la.Gender = a.Gender;
+                la.Address = a.Address;
+                l.Add(la);
+            }
+            return View(l);
+
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employeemodel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                DB56Entities db = new DB56Entities();
+                Employee la = new Employee();
+                la.CNIC = model.CNIC;
+                la.Name = model.Name;
+                la.Password = model.Password;
+                la.Email = model.Email;
+                la.Salary = model.Salary;
+                la.Gender = model.Gender;
+                la.Address = model.Address;
+
+                db.Employees.Add(la);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(string id)
+        {
+            DB56Entities db = new DB56Entities();
+            var a = db.Employees.Where(x => x.CNIC ==  id).SingleOrDefault();
+            var result = db.Employees.Remove(a);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+               
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ELMS/ELMS/Models/Employeemodel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoanManagementSystem.Models
+{
+    public class Employeemodel
+    {
+        [Required]
+        [Display(Name = "CNIC")]
+        public string CNIC { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Salary")]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+    }
+}
